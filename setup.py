3acc871afcd5832d44a37177708e511a800c690c
Jordan Bramble <jordanbramble@gmail.com>
2016-09-21 16:25:05 -0400

    prepare for release on PIP

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='cavaconn',
       version='0.1',
-      description='Connect to all of our shit for twitter API, or SQL inserts',
+      description='Create database engines for SQL servers, and easily connect to APIs',
       url='https://github.com/cavagrill/cava_connect',
       author='Jordan Bramble',
       author_email='jordan.bramble@cavagrill.com',
